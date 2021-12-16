be0e56e9b9e776197af67f3da934f14920f6f368
Shreyan Mohanty <shreyanmohanty4@gmail.com>
2021-12-16 20:07:35 +0530

    Added Image Upload Page

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,4 +1,7 @@
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
+import 'package:file_picker/file_picker.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,26 +14,17 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Spliit - Cost splitting made easy',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.purple,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Get Started'),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({Key? key, required this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -39,68 +33,123 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
+  int _numPeople = 2;
+  Map<int, String> _people = {};
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            DropdownButton(
+                value: _numPeople,
+                items: const [
+                  DropdownMenuItem(
+                    child: Text("Two People"),
+                    value: 2,
+                  ),
+                  DropdownMenuItem(
+                    child: Text("Three People"),
+                    value: 3,
+                  )
+                ],
+                onChanged: (int? value) {
+                  setState(() {
+                    _numPeople = value!;
+                  });
+                },
+                hint: const Text("Party Size")),
+            for (int i = 0; i < _numPeople; ++i)
+              TextField(
+                  decoration: InputDecoration(labelText: 'Person ${i + 1}'),
+                  onChanged: (String value) {
+                    _people[i + 1] = value;
+                  }),
+            ElevatedButton.icon(
+              onPressed: () => Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => const PageTwo())),
+              label: const Text("Next"),
+              icon: const Icon(
+                Icons.arrow_forward_rounded,
+                size: 16,
+              ),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+}
+
+class PageTwo extends StatefulWidget {
+  const PageTwo({Key? key}) : super(key: key);
+
+  @override
+  State<PageTwo> createState() => _PageTwoState();
+}
+
+class _PageTwoState extends State<PageTwo> {
+  String endpoint =
+      'https://spliit-form-recognizer.cognitiveservices.azure.com/formrecognizer/v2.1/prebuilt/receipt/analyze';
+  Uint8List? receipt;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Upload Receipt"),
+      ),
+      body: Center(
+        child: Column(children: <Widget>[
+          Container(
+              child: receipt == null
+                  ? const Text('No Image Chosen')
+                  : Image.memory(
+                      receipt!,
+                      width: 200,
+                      height: 300,
+                    )),
+          ElevatedButton.icon(
+            onPressed: () async {
+              var image = await FilePicker.platform.pickFiles();
+              if (image != null) {
+                setState(() {
+                  receipt = image.files.first.bytes;
+                });
+              }
+            },
+            icon: const Icon(Icons.camera_enhance_outlined, size: 16),
+            label: const Text("Choose Image"),
+          ),
+          ElevatedButton.icon(
+            onPressed: () => Navigator.push(context,
+                MaterialPageRoute(builder: (context) => const PageThree())),
+            label: const Text("Next"),
+            icon: const Icon(
+              Icons.arrow_forward_rounded,
+              size: 16,
+            ),
+          ),
+        ]),
+      ),
+    );
+  }
+}
+
+class PageThree extends StatefulWidget {
+  const PageThree({Key? key}) : super(key: key);
+
+  @override
+  State<PageThree> createState() => _PageThreeState();
+}
+
+class _PageThreeState extends State<PageThree> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
   }
 }
